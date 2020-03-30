04bdec7162dfb1004c4580b6a10f39b1155004ee
EsunR <641411169@qq.com>
2020-03-30 21:49:45 +0800

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export function getBookTypeCode(type: string): string {
+export function getBookTypeCode(type: string): string | undefined {
   let dictionary = `
     专著M；报纸N；期刊J；专利文献P；汇编G；古籍O；技术标准S；
     学位论文D；科技报告R；参考工具K；检索工具W；档案B；录音带A；
@@ -6,10 +6,10 @@ export function getBookTypeCode(type: string): string {
     乐谱I；电影片Y；手稿H；微缩胶卷U；幻灯片Z；微缩平片F；其他E
   `
   let dictionaryArr = dictionary.split("；")
-  let result = dictionaryArr.find(dicItem => {
+  let result = dictionaryArr.find((dicItem: string) => {
     return new RegExp(type).test(dicItem) ? dicItem : ""
   })
-  return result.trim()
+  return result?.trim()
 }
 
 console.log(getBookTypeCode("专著"))

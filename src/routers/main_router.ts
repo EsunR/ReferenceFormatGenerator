54e5d1aa06d8fe72709473002f0f09be812f722b
EsunR <641411169@qq.com>
2020-03-31 10:52:24 +0800

    update: 接口编写

--- src/routers/main_router.ts
+++ src/routers/main_router.ts
@@ -3,14 +3,10 @@ import Router from "koa-router"
 import axios from "axios"
 import cheerio from "cheerio"
 
-const router: Router = new Router()
+// utils
+import { getChildText, getBookTypeCode } from "../utils"
 
-function getChildText(cheerio: Cheerio, selector: string): string {
-  return cheerio
-    .find(selector)
-    .text()
-    .trim()
-}
+const router: Router = new Router()
 
 router.get("/test", async ctx => {
   ctx.body = new ResBody({
@@ -19,26 +15,39 @@ router.get("/test", async ctx => {
 })
 
 router.get("/find", async ctx => {
-  const { bookName: searchName } = ctx.query
-  const host: string = encodeURI(
-    `http://find.nlc.cn/search/doSearch?query=${searchName}&secQuery=&actualQuery=${searchName}&searchType=2&docType=全部&isGroup=isGroup&targetFieldLog=全部字段&orderBy=RELATIVE`
-  )
-  let htmlContent: string = (await axios.get(host)).data
-  const $ = cheerio.load(htmlContent)
-  const bookDom = $(
-    "#wj_content > div > div.search_information > div.article_list > div:nth-child(1)"
-  )
-  let bookId = bookDom.find(".book_name a").attr("id")
-  let result = {
-    id: bookId,
-    name: getChildText(bookDom, ".book_name a"),
-    sort: getChildText(bookDom, ".book_type:nth-child(2) > .book_val"),
-    author: getChildText(bookDom, ".book_type:nth-child(3) > .book_val"),
-    year: getChildText(bookDom, ".book_type:nth-child(4) > .book_val"),
-    publish: getChildText(
-      bookDom,
-      ".book_type:nth-child(4) > .book_she > .book_val"
+  let searchNames: string[] = ctx.query
+  let result: any[] = []
+  for (let key in searchNames) {
+    if (!/book/.test(key)) {
+      continue
+    }
+    let searchName = searchNames[key]
+    const host: string = encodeURI(
+      `http://find.nlc.cn/search/doSearch?query=${searchName}&secQuery=&actualQuery=${searchName}&searchType=2&docType=全部&isGroup=isGroup&targetFieldLog=全部字段&orderBy=RELATIVE`
     )
+    let htmlContent: string = (await axios.get(host)).data
+    const $ = cheerio.load(htmlContent)
+    const bookDom = $(
+      "#wj_content > div > div.search_information > div.article_list > div:nth-child(1)"
+    )
+    let bookId = bookDom.find(".book_name a").attr("id")
+    let bookInfo = {
+      id: bookId ? bookId : new Date().valueOf().toString(),
+      searchName: searchName,
+      name: getChildText(bookDom, ".book_name a"),
+      sort: getChildText(bookDom, ".book_type:nth-child(2) > .book_val"),
+      sortCode: getBookTypeCode(
+        getChildText(bookDom, ".book_type:nth-child(2) > .book_val")
+      ),
+      author: getChildText(bookDom, ".book_type:nth-child(3) > .book_val"),
+      year: getChildText(bookDom, ".book_type:nth-child(4) > .book_val"),
+      publish: getChildText(
+        bookDom,
+        ".book_type:nth-child(4) > .book_she > .book_val"
+      ),
+      success: bookId ? true : false
+    }
+    result.push(bookInfo)
   }
   ctx.body = new ResBody({
     data: result
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,15 +1,22 @@
-export function getBookTypeCode(type: string): string | undefined {
-  let dictionary = `
-    专著M；报纸N；期刊J；专利文献P；汇编G；古籍O；技术标准S；
-    学位论文D；科技报告R；参考工具K；检索工具W；档案B；录音带A；
-    图表Q；唱片L；产品样本X；录相带V；会议录C；中译文T；
-    乐谱I；电影片Y；手稿H；微缩胶卷U；幻灯片Z；微缩平片F；其他E
-  `
-  let dictionaryArr = dictionary.split("；")
-  let result = dictionaryArr.find((dicItem: string) => {
+export function getBookTypeCode(type: string): string | null {
+  if (!type) {
+    return ""
+  }
+  let dictionary: string =
+    "专著M；报纸N；期刊J；专利文献P；汇编G；古籍O；技术标准S；学位论文D；科技报告R；参考工具K；检索工具W；档案B；录音带A；图表Q；唱片L；产品样本X；录相带V；会议录C；中译文T；乐谱I；电影片Y；手稿H；微缩胶卷U；幻灯片Z；微缩平片F；其他E"
+  let dictionaryArr: string[] = dictionary.split("；")
+  let result: string | undefined = dictionaryArr.find((dicItem: string) => {
     return new RegExp(type).test(dicItem) ? dicItem : ""
   })
-  return result?.trim()
+  result = result ? result.trim() : "其他E"
+  let reg: RegExp = new RegExp("[A-Z]")
+  let regResult: any[] | null = reg.exec(result)
+  return regResult ? regResult[0] : "E"
 }
 
-console.log(getBookTypeCode("专著"))
+export function getChildText(cheerio: Cheerio, selector: string): string {
+  return cheerio
+    .find(selector)
+    .text()
+    .trim()
+}
